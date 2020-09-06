c64adea43ebdd43f1dcffc04764871f38c1c72db
Harrison Shoebridge <harrison@theshoebridges.com>
2020-09-07 09:03:11 +1000

    Set up ass-3

--- sketch.js
+++ sketch.js
@@ -1,28 +1,15 @@
 function setup() {
-  // create the canvas (800px wide, 600px high)
-  createCanvas(800, 600);
-
-  // make the text nice and big - adjust the size parameter
-  // to make *your* name fit nicely on the nametag
-  textSize(150);
-
-  // draw a border to help you see the size
-  // this isn't compulsory (remove this code if you like)
-  strokeWeight(5);
-  rect(0, 0, width, height);
+  // add your setup code here
 }
 
 function draw() {
-  // your cool nametag code goes in this draw function
-
-  // replace "Name" with your name!
-  text("Name", 100, height-100);
+  // add your draw code here
 }
 
 // when you hit the spacebar, what's currently on the canvas will be saved (as a
-// "nametag.png" file) to your downloads folder
+// "thumbnail.png" file) to your downloads folder
 function keyTyped() {
   if (key === " ") {
-    saveCanvas("nametag.png");
+    saveCanvas("thumbnail.png");
   }
 }
