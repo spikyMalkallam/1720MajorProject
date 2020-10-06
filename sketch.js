a2b271fb9eb2424746af4d020f0a5e2705c5fd87
Harrison Shoebridge <harrison@theshoebridges.com>
2020-10-07 10:45:25 +1100

    Add better note to the thumbnail download code

--- sketch.js
+++ sketch.js
@@ -10,8 +10,13 @@ function draw() {
   // add your draw code here
 }
 
-// when you hit the spacebar, what's currently on the canvas will be saved (as a
-// "thumbnail.png" file) to your downloads folder
+// when you hit the spacebar, what's currently on the canvas will be saved (as
+// a "thumbnail.png" file) to your downloads folder. this is a good starting
+// point for the final thumbnail of your project (this allows us to make a
+// showcase of everyone's work like we did for the nametag assignment).
+//
+// remember that you need to resize the file to 1280x720, and you will probably
+// want to delete this bit for your final submission.
 function keyTyped() {
   if (key === " ") {
     saveCanvas("thumbnail.png");
