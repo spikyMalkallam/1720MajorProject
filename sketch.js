1675869e58972ac8462248e0601ba53478680fca
u7310220 <u7310220@anu.edu.au>
2021-10-20 14:08:12 +1100

    Stuff\

--- sketch.js
+++ sketch.js
@@ -15,10 +15,13 @@ let gravity = 0;
 let angle = 0;
 let seperation = 0;
 
-cellestialBodies = [];
+let cellestialBodies = [];
+let trajectories = [];
 
 class cellestialBody {
-  constructor(x,y,mass,diameter) {
+  constructor(name,x,y,mass,diameter) {
+    //Name of the body
+    this.name = name;
     //Position of body
     this.pos = createVector(x, y);
     //Weight of the body
@@ -29,44 +32,67 @@ class cellestialBody {
     this.density = mass/((4/3)*PI*((diameter/2)**3));
     //Force on the body
     this.forceVector = createVector(0, 0);
+    //Acceleration of object
+    this.accelerationVector = createVector(0, 0);
     //Velocity of object
     this.velocityVector = createVector(0, 0);
   }
+}
+
+function setPosition(body, x, y) {
+  body.pos.x = x;
+  body.pos.y = y;
+}
+
+function setAcceleration(body, x ,y) {
+  body.accelerationVector.x = x;
+  body.accelerationVector.y = y;
+}
+
+function setVelocity(body, x, y) {
+  body.velocityVector.x = x;
+  body.velocityVector.y = y;
 }
 
 function angleBetween(point1, point2) {
   return degrees(Math.atan((point2.y-point1.y)/(point2.x-point1.x)));
 }
 function getForces(body) {
+  body.accelerationVector = createVector(0,0);  
   for (let bodyArray of cellestialBodies) {
     if (body == bodyArray) {
       continue;
     }
+    angleMode(DEGREES);
     seperation = (dist(body.pos.x, body.pos.y, bodyArray.pos.x, bodyArray.pos.y));
     gravity = (((6.67 * 10)**-11) * body.mass * bodyArray.mass)/seperation**2;
     angle = angleBetween(createVector(body.pos.x, body.pos.y),createVector(bodyArray.pos.x, bodyArray.pos.y));
+    seperationV = createVector((Math.sin(angle)*seperation), (Math.cos(angle)*seperation))
     body.forceVector = createVector(0,0);
-    body.forceVector.x += Math.sin(angle)*gravity;
-    body.forceVector.y += Math.cos(angle)*gravity;
+    body.forceVector.x += Math.cos(angle*(Math.PI/180))*gravity;
+    body.forceVector.y += Math.sin(angle*(Math.PI/180))*gravity;
     //console.log(body.forceVector.x);
     //console.log(body.forceVector.y);
     //console.log(seperation);
     //Converting to force velcity, divided by thirty to adjust from m/s to m/(s/30) 
-    //********************CHANGE BACK TO 30 FOR REAL TIME***************************  <here>                                                    
-    body.velocityVector.x = (Math.sqrt((body.forceVector.x*seperation)/(body.mass/2)))/30;
-    body.velocityVector.y = (Math.sqrt((body.forceVector.y*seperation)/(body.mass/2)))/30;
+    //********************CHANGE BACK TO 30 FOR REAL TIME***************************                                                
+    body.accelerationVector.x += (body.forceVector.x/body.mass)*-1;
+    body.accelerationVector.y += (body.forceVector.y/body.mass)*-1;
+    console.log(body.name+body.forceVector.x);
     //
-    console.log((body.velocityVector.x)/1000000);
-    console.log(body.pos.x);
+    //console.log(body.pos.x/3);
   }
+  text("Gravitation pull on moon: "+gravity+"N", earth.pos.x, earth.pos.y+100);
+  body.velocityVector.x += body.accelerationVector.x*100;
+  body.velocityVector.y += body.accelerationVector.y*100;
 }
 
 function updatePositions() {
   for (let body of cellestialBodies) {
     getForces(body);
     //Velocity is in m so must be divided by a million to convert to 1000km
-    body.pos.x += (body.velocityVector.x)/1000000;
-    body.pos.y += (body.velocityVector.y)/1000000;
+    body.pos.x += (body.velocityVector.x)/100000;
+    body.pos.y += (body.velocityVector.y)/100000;
   }
 }
 
@@ -82,18 +108,42 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  moon = new cellestialBody(500,500,(7.35*10**22),17370);
+  frameRate(60);
+
+  moon = new cellestialBody("Moon", 892,116,(7.35*10**22),17370);
   cellestialBodies.push(moon);
-  earth = new cellestialBody(884,500,(6*10**24),63710);
+  setAcceleration(moon, 0, 0);
+  earth = new cellestialBody("Earth", 892,500,(6*10**24),63710);
   cellestialBodies.push(earth);
-  frameRate(30);
+  
+  setVelocity(moon, 35230,0);
 }
 
 function draw() {
-  angleMode(DEGREES);
   background("black");
-  updatePositions();
+  //noFill();
+  fill("black");
+  stroke("white");
+  color("white");
+  ellipse(earth.pos.x, earth.pos.y, 384*2);
+  color("white");
+  fill("white");
   drawBodies();
+  updatePositions();
+  text("Moon x velocity: "+moon.velocityVector.x, 400, 400);
+  text("Moon y velocity: "+moon.velocityVector.y, 400, 450);
+  text("Moon x acceleration: "+moon.accelerationVector.x, 400, 420);
+  text("Moon y acceleration: "+moon.accelerationVector.y, 400, 470);
+  line(moon.pos.x, moon.pos.y)
+  //text("Moon gravity: "+moon.accelerationVector.y, earth.pos.x, earth.pos.y+120);
+  //text("Moon gravity: "+moon.accelerationVector.y, earth.pos.x, earth.pos.y+140);
+  //text("Moon gravity: "+moon.accelerationVector.y, earth.pos.x, earth.pos.y+160);
+  line(moon.pos.x, moon.pos.y, earth.pos.x, earth.pos.y);
+  trajectories.push(moon.pos.x, moon.pos.y, 10);
+  for (i = 0; i < trajectories.length; i += 3) {
+    fill("orange");
+    ellipse(trajectories[i], trajectories[i+1], trajectories[i+2]);
+  }
 }
 
 // when you hit the spacebar, what's currently on the canvas will be saved (as
