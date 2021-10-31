cab7be0b1e0b655a5de3882b32db6af7e1a8d5a9
u7310220 <u7310220@anu.edu.au>
2021-11-01 03:39:49 +1100

    Weighten and lighten with insert statered

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-
-
+//Scroll Wheel Recorder
 window.addEventListener("wheel", function(e) {
   if (e.deltaY > 0)
     sf *= 0.95;
@@ -26,7 +25,6 @@ let trajectories = [];
 let camera = {
   xOffset: -966,
   yOffset: -473,
-  zoom: 0
 }
 
 class cellestialBody {
@@ -75,6 +73,17 @@ function getPosY(body) {
   return body.pos.y;
 }
 
+function changeMassPostive(body) {
+  body.mass += body.mass/10;
+}
+
+function changeMassNegative(body) {
+  body.mass -= body.mass/10;
+  if (body.mass < 0) {
+    body.mass = 0;
+  }
+}
+
 let equalise1 = 0;
 let equalise2 = 1;
 function angleBetween(point1, point2) {
@@ -94,7 +103,7 @@ function getForces(body) {
     seperation = (dist(body.pos.x, body.pos.y, bodyArray.pos.x, bodyArray.pos.y));
     gravity = ((((6.67 * 10)**-11) * body.mass * bodyArray.mass)/seperation**2)**1; //Adjust gravity strength
     angle = angleBetween(createVector(body.pos.x, body.pos.y),createVector(bodyArray.pos.x, bodyArray.pos.y));
-    seperationV = createVector((Math.sin(angle)*seperation), (Math.cos(angle)*seperation))
+    //seperationV = createVector((Math.sin(angle)*seperation), (Math.cos(angle)*seperation))
     body.forceVector = createVector(0,0);
     body.forceVector.x += Math.cos(angle*(Math.PI/180))*gravity;
     body.forceVector.y += Math.sin(angle*(Math.PI/180))*gravity;
@@ -121,11 +130,83 @@ function updatePositions() {
 let mousePosX = 0;
 let mousePosY = 0;
 
+let unPressed = [77, 78, 80];
+let pressed = [164, 167, 171];
+
+let button1 = [77, 78, 80];
+let button1Pressed = false;
+let button2 = [77, 78, 80];
+let button2Pressed = false;
+let button3 = [77, 78, 80];
+let button3Pressed = false;
+let button4 = [77, 78, 80];
+let button4Pressed = false;
+
+let planetSelected = null;
+
 function mousePressed() {
+  //Button Detection
+  if(mouseX > (width/1.067853170189099+width/30)-width/26/2 && mouseX < (width/1.067853170189099+width/30)+width/26/2 &&
+    mouseY < (height/10+height/15)+width/26/2 && mouseY > (height/10+height/15)-width/26/2) 
+    {
+      console.log("1");
+      button1Pressed = !button1Pressed;
+      button2Pressed = false;
+      button3Pressed = false;
+      button4Pressed = false;
+  };
+  if(mouseX > (width/1.067853170189099+width/30)-width/26/2 && mouseX < (width/1.067853170189099+width/30)+width/26/2 &&
+    mouseY < (height/10+height/15+height/5.7)+width/26/2 && mouseY > (height/10+height/15+height/5.7)-width/26/2) 
+    {
+      console.log("1");
+      button2Pressed = !button2Pressed;
+      button1Pressed = false;
+      button3Pressed = false;
+      button4Pressed = false;
+  };
+  if(mouseX > (width/1.067853170189099+width/30)-width/26/2 && mouseX < (width/1.067853170189099+width/30)+width/26/2 &&
+    mouseY < (height/10+height/15+(height/5.5)*2)+width/26/2 && mouseY > (height/10+height/15+(height/5.5)*2)-width/26/2) 
+    {
+      console.log("1");
+      button3Pressed = !button3Pressed;
+      button1Pressed = false;
+      button2Pressed = false;
+      button4Pressed = false;
+  };
+  if(mouseX > (width/1.067853170189099+width/30)-width/26/2 && mouseX < (width/1.067853170189099+width/30)+width/26/2 &&
+    mouseY < (height/10+height/15+(height/5.4)*3)+width/26/2 && mouseY > (height/10+height/15+(height/5.4)*3)-width/26/2) 
+    {
+      console.log("1");
+      button4Pressed = !button4Pressed;
+      button1Pressed = false;
+      button2Pressed = false;
+      button3Pressed = false;
+  };
+
+
+  for (let body of cellestialBodies) {
+    if (mouseX > (((body.pos.x*sf)-camera.xOffset)-(body.diameter/30/2)*sf) && mouseX < (((body.pos.x*sf)-camera.xOffset)+(body.diameter/30/2)*sf) &&
+        mouseY < (((body.pos.y*sf)-camera.yOffset)+(body.diameter/30/2)*sf) && mouseY > (((body.pos.y*sf)-camera.yOffset)-(body.diameter/30/2)*sf)) {
+          if (button1Pressed || button2Pressed) {
+          console.log("Plamet");
+          planetSelected = body;
+          }
+        }
+  }
+  //ellipse((((earth.pos.x*sf)-camera.xOffset)-(earth.diameter/30/2)*sf), (((earth.pos.y*sf)-camera.yOffset)+(earth.diameter/30/2)*sf), 10);
+  //ellipse((((earth.pos.x*sf)-camera.xOffset)+(earth.diameter/30/2)*sf), (((earth.pos.y*sf)-camera.yOffset)-(earth.diameter/30/2)*sf), 10);
+
+
+
   mousePosX = mouseX;
   mousePosY = mouseY;
   mouseOffX = camera.xOffset;
   mouseOffy = camera.yOffset;
+  //console.log(width/mouseX+", "+height/mouseY);
+}
+
+function mouseReleased() {
+  planetSelected = null;
 }
 
 function mouseDragged() {
@@ -139,8 +220,7 @@ function mouseDragged() {
 
 let bodyMomentumX;
 let bodyMomentumY;
-let removeBodies = []
-let cheese = [];
+let removeBodies = [];
 
 function collisionDetection() {
   for (let body of cellestialBodies) {
@@ -175,6 +255,93 @@ function removeVal(arr, value) {
     }
   }
   return arr2;
+}
+
+//v = d/t
+// T**2 = ((4*Math.PI)/6.67*10**-11*(centre mass + planet))*r**3
+
+let orbitalTime;
+let orbitalVelocity;
+
+//Two bodies
+function orbitSpeed(body,bodyArray) {
+  //orbitalTime = ((4*Math.PI)/6.67*10**-11*(centre.mass+ planet.mass))*(dist(centre.pos.x,centre.pos.y,planet.pos.x,planet.pos.y)**3);
+  //orbitalVelocity = dist(centre.pos.x,centre.pos.y,planet.pos.x,planet.pos.y)/orbitalTime;
+  angleMode(DEGREES);
+  seperation = (dist(body.pos.x, body.pos.y, bodyArray.pos.x, bodyArray.pos.y));
+  gravity = ((((6.67 * 10)**-11) * body.mass * bodyArray.mass)/seperation**2)**1; //Adjust gravity strength
+  angle = angleBetween(createVector(body.pos.x, body.pos.y),createVector(bodyArray.pos.x, bodyArray.pos.y));
+  //seperationV = createVector((Math.sin(angle)*seperation), (Math.cos(angle)*seperation))
+  body.forceVector = createVector(0,0);
+  body.forceVector.x += Math.cos(angle*(Math.PI/180))*gravity;
+  body.forceVector.y += Math.sin(angle*(Math.PI/180))*gravity;
+  return body.forceVector;
+}
+
+function userInterface() {
+  stroke(129, 129, 129,10);
+  fill(129, 129, 129,30);
+  rect(width/1.067853170189099, height/10, width/13,height/1.25);
+  if (button1Pressed) {
+    button1 = pressed;
+  }
+  else {
+    button1 = unPressed;
+  }
+  if (button2Pressed) {
+    button2 = pressed;
+  }
+  else {
+    button2 = unPressed;
+  }
+  if (button3Pressed) {
+    button3 = pressed;
+  }
+  else {
+    button3 = unPressed;
+  }
+  if (button4Pressed) {
+    button4 = pressed;
+  }
+  else {
+    button4 = unPressed;
+  }
+  //Button 1 Draw
+  fill(button1[0], button1[1], button1[2]);
+  ellipse(width/1.067853170189099+width/30, height/10+height/15, width/26);
+  //Button 2 Draw
+  fill(button2[0], button2[1], button2[2]);
+  ellipse(width/1.067853170189099+width/30, height/10+height/15+height/5.7, width/26);
+  //Button 3 Draw
+  fill(button3[0], button3[1], button3[2]);
+  ellipse(width/1.067853170189099+width/30, height/10+height/15+(height/5.5)*2, width/26);
+  //Button 4 Draw
+  fill(button4[0], button4[1], button4[2]);
+  ellipse(width/1.067853170189099+width/30, height/10+height/15+(height/5.4)*3, width/26);
+  //Labels
+  stroke("Black");
+  fill("Black");
+  textSize(width/120);
+  text("Heavier", width/1.067853170189099+width/52,height/10+height/14);
+  if (button1Pressed || button2Pressed) {
+    textSize(width/60);
+    stroke(129, 129, 129);
+    fill(129, 129, 129);
+    text("Click and hold on a planet", width/2.5,height/1.05);
+    stroke("Black");
+    fill("Black");
+    textSize(width/120);
+  }
+  text("Lighten", width/1.067853170189099+width/51,height/4+height/10.2);
+  text("Orbit", width/1.067853170189099+width/43,height/2+height/28);
+  text("Insert", width/1.067853170189099+width/44,height/1.45+height/28);
+  if (button4Pressed) {
+    spawnMenu();
+  }
+}
+
+function spawnMenu() {
+  rect()
 }
 
 function drawBodies() {
@@ -229,13 +396,14 @@ function drawBodies() {
       stroke(95, 115, 159);
       fill(95, 115, 159);
     }
-    if (body == ball) {
-      stroke("white");
-      fill("white");
-    }
+    //if (body == ball) {
+     // stroke("white");
+     //fill("white");
+    //}
     //Velocity Vector
     //line((body.pos.x*sf)-camera.xOffset, (body.pos.y*sf)-camera.yOffset,((body.pos.x*sf)-camera.xOffset)+body.velocityVector.x, ((body.pos.y*sf)-camera.yOffset)+body.velocityVector.y)
     ellipse((body.pos.x*sf)-camera.xOffset, (body.pos.y*sf)-camera.yOffset, (body.diameter/30)*sf);
+    console.log(planetSelected);
   }
 }
 
@@ -259,8 +427,8 @@ function setup() {
   earth = new cellestialBody("Earth", 1000,58430,(6*10**24),73710,"planet");
   cellestialBodies.push(earth);
 
-  ball = new cellestialBody("Ball", -10000,58430,(9*10**24),73710,"planet");
-  cellestialBodies.push(ball);
+  //ball = new cellestialBody("Ball", -10000,58430,(9*10**24),73710,"planet");
+  //cellestialBodies.push(ball);
 
   //moon = new cellestialBody("Moon", 1000,108064,(7.35*10**22),17370);
   //cellestialBodies.push(moon);
@@ -281,23 +449,43 @@ function setup() {
   cellestialBodies.push(neptune);
 
   //Putting each planet into Orbit
-  //setVelocity(mercury, -4950000,0);
+  setVelocity(mercury, -4950000,0);
+  console.log("Mercury: "+orbitSpeed(sun,mercury)+" Expected: -4950000");
   setVelocity(venus, -4200000,0);
+  console.log("Earth: "+orbitSpeed(sun,earth)+" Expected: -3425000.2222");
   setVelocity(earth, -3425000.2222 ,0);
   //setVelocity(moon, -2227220.2222,0);
+  console.log("Mars: "+orbitSpeed(sun,mars)+" Expected: -3000000");
   setVelocity(mars, -3000000,0);
+  console.log("Juipiter: "+orbitSpeed(sun,jupiter)+" Expected: -2830000");
   setVelocity(jupiter, -2830000,0);
   setVelocity(saturn, -2630000,0);
   setVelocity(uranus, -2500000,0);
   setVelocity(neptune, -2299000,0);
-  setVelocity(ball, 3425000.2222,0);
+  //setVelocity(ball, 3425000.2222,0);
 }
 
 function draw() {
   background("black");
   drawBodies();
+  userInterface();
   updatePositions();
   collisionDetection();
+  //Weighten
+  if (button1Pressed) {
+    if (planetSelected != null) {
+      console.log("Benuis");
+      changeMassPostive(planetSelected);
+    }
+  }
+  //Lighten
+  if (button2Pressed) {
+    if (planetSelected != null) {
+      console.log("Benuis");
+      changeMassNegative(planetSelected);
+    }
+  }
+
   //console.log(camera.xOffset+", "+camera.yOffset);
   //console.log(sf);
 }
